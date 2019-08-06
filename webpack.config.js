98470dc966c3b1152d4858d9906c6f066e068e8a
Tyler Hawkins <thawkins@instructure.com>
2019-08-06 17:01:51 -0600

    - Moved index.html file into the "src" directory - Installed 'html-webpack-plugin' - Added html-webpack-plugin to the webpack config so that it manages the index.html file for us - Now if we change what our bundle is called, the index.html file gets updated automatically for us

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -8,6 +9,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
   ]
 }
