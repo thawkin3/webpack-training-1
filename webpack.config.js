7c3075fd240cb527e78b057732542cc12bb27b49
Tyler Hawkins <thawkins@instructure.com>
2019-08-06 16:57:32 -0600

    - Added the clean-webpack-plugin to remove the "dist" directory each time we build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 }
