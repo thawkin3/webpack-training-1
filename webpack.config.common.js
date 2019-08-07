9c7e2571d89ee203d7e6479a15888f6dd44b670c
Tyler Hawkins <thawkins@instructure.com>
2019-08-07 10:05:23 -0600

    - Updated output to include the contenthash for cache busting when new files are written. `filename: '[name].[contenthash].js'`

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
