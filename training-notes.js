b480fd0295bea7f058c3290a430a3815a4da96bd
Tyler Hawkins <thawkins@instructure.com>
2019-08-07 10:20:11 -0600

    - Added training notes for following along during the presentation

---
+++ training-notes.js
@@ -0,0 +1,516 @@
+////////////////////////////////////////////////////////
+// simple config
+////////////////////////////////////////////////////////
+
+// package.json
+// "build": "webpack --config=webpack.config.js"
+
+
+// webpack.config.js
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
+
+
+
+////////////////////////////////////////////////////////
+// clean-webpack-plugin
+////////////////////////////////////////////////////////
+
+// webpack.config.js
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
+
+
+
+////////////////////////////////////////////////////////
+// html-webpack-plugin
+////////////////////////////////////////////////////////
+
+// move index.html file into the src directory
+
+// webpack.config.js
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ]
+}
+
+
+
+////////////////////////////////////////////////////////
+// prod and dev configs, webpack-dev-server
+////////////////////////////////////////////////////////
+
+// webpack.config.dev.js
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ]
+}
+
+
+// webpack.config.prod.js
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ]
+}
+
+
+// package.json
+// "build": "webpack --config=webpack.config.prod.js",
+// "build-dev": "webpack --config=webpack.config.dev.js",
+// "start": "webpack-dev-server --config=webpack.config.dev.js --open"
+
+
+
+////////////////////////////////////////////////////////
+// webpack-merge
+////////////////////////////////////////////////////////
+
+// webpack.config.common.js
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ]
+}
+
+
+// webpack.config.dev.js
+const merge = require('webpack-merge')
+const commonConfig = require('./webpack.config.common')
+
+module.exports = merge(commonConfig, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+})
+
+
+// webpack.config.prod.js
+const merge = require('webpack-merge')
+const commonConfig = require('./webpack.config.common')
+
+module.exports = merge(commonConfig, {
+  mode: 'production',
+  devtool: 'source-map',
+})
+
+
+
+////////////////////////////////////////////////////////
+// loaders (style-loader, css-loader)
+////////////////////////////////////////////////////////
+
+// index.css
+`body {
+  background: deeppink;
+  color: white;
+}`
+
+
+// index.js
+import './index.css'
+
+
+// webpack.config.common.js
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
+
+
+
+////////////////////////////////////////////////////////
+// loaders (babel-loader)
+////////////////////////////////////////////////////////
+
+// Setup here: https://babeljs.io/setup#installation
+
+// .babelrc
+{
+  "presets": ["@babel/preset-env"]
+}
+
+
+// index.js
+import './index.css'
+
+const p = document.createElement('p')
+p.textContent = 'Hello from webpack!'
+document.body.appendChild(p)
+
+const p2 = document.createElement('p')
+const numbers1 = [1, 2, 3, 4, 5, 6]
+const numbers2 = [7, 8, 9, 10]
+const numbers3 = [...numbers1, ...numbers2]
+p2.textContent = numbers3.join(' ')
+document.body.appendChild(p2)
+
+
+// webpack.config.common.js
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /[\\/]node_modules[\\/]/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ]
+  }
+}
+
+
+
+////////////////////////////////////////////////////////
+// mini-css-extract-plugin
+////////////////////////////////////////////////////////
+
+// webpack.config.common.js (remove the CSS loaders)
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /[\\/]node_modules[\\/]/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ]
+  }
+}
+
+
+// webpack.config.dev.js (moved the CSS loaders we had to this file)
+const merge = require('webpack-merge')
+const commonConfig = require('./webpack.config.common')
+
+module.exports = merge(commonConfig, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+    ]
+  }
+})
+
+
+// webpack.config.prod.js (use MiniCssExtractPlugin)
+const merge = require('webpack-merge')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const commonConfig = require('./webpack.config.common')
+
+module.exports = merge(commonConfig, {
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ]
+})
+
+
+
+////////////////////////////////////////////////////////
+// cache busting
+////////////////////////////////////////////////////////
+
+// webpack.config.common.js
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js', // this line is the only difference
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /[\\/]node_modules[\\/]/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ]
+  }
+}
+
+
+
+////////////////////////////////////////////////////////
+// minify CSS for production
+////////////////////////////////////////////////////////
+
+// webpack.config.prod.js
+const merge = require('webpack-merge')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const commonConfig = require('./webpack.config.common')
+
+module.exports = merge(commonConfig, {
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin({
+        cssProcessorOptions: {
+          map: {
+            inline: false,
+            annotation: true,
+          },
+        },
+      }),
+    ],
+  },
+})
+
+
+
+////////////////////////////////////////////////////////
+// minify CSS and JS for production
+////////////////////////////////////////////////////////
+
+// webpack.config.prod.js
+const merge = require('webpack-merge')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const commonConfig = require('./webpack.config.common')
+
+module.exports = merge(commonConfig, {
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin({
+        cssProcessorOptions: {
+          map: {
+            inline: false,
+            annotation: true,
+          },
+        },
+      }),
+      new TerserPlugin({
+        // Use multi-process parallel running to improve the build speed
+        // Default number of concurrent runs: os.cpus().length - 1
+        parallel: true,
+        // Enable file caching
+        cache: true,
+        sourceMap: true,
+      }),
+    ],
+  },
+})
+
+
